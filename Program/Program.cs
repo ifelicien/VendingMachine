61f17bbad2813dd682d8d5a4a6a0d4b7a04ec0fb
ifeli <felicienirwin>
2024-05-09 08:27:41 -0500

    added logic to helper functions, comment out display function until others are fully functional

--- Program/Program.cs
+++ Program/Program.cs
@@ -3,6 +3,7 @@ using Currency;
 class Program{
     
     public static void Main(string[] args){
+        List<Item> items = new List<Item> {};
 
         //create products 
         Item coke = new Item("Coke", .95, 30);
@@ -19,41 +20,48 @@ class Program{
 
         Console.WriteLine("Enter item number: ");
         string input = Console.ReadLine();
-        int selection = int.Parse(input);
+        if(int.TryParse(input, out int selection)){
 
-        switch (selection){
-        
-        case 1:
-            Console.WriteLine($"{coke.GetName()}.");
-            double ItemCost = coke.GetPrice();
+            switch (selection){
+            
+            case 1:
+                items.Add(coke);
+                Console.WriteLine($"{coke.GetName()}.");
+                double ItemCost = coke.GetPrice();
+                break;
+
+            case 2:
+                items.Add(doritos);
+                Console.WriteLine($"{doritos.GetName()}.");
+                break;
+
+            case 3:
+                items.Add(snickers);
+                Console.WriteLine($"{snickers.GetName()}.");
+                break;
+
+            case 4:
+                items.Add(chexMix);
+                Console.WriteLine($"{chexMix.GetName()}.");
+                break;
+
+            case 5:
+                items.Add(pepsi);
+                Console.WriteLine($"{pepsi.GetName()}.");
+                break;
+            case 6:
+                DisplayTotals();
+                break;
+
+            default:
+            Console.WriteLine("Invalid input");
             break;
-        
-        case 2:
-            Console.WriteLine($"{doritos.GetName()}.");
-            break;
-        
-        case 3:
-            Console.WriteLine($"{snickers.GetName()}.");
-            break;
-        
-        case 4:
-            Console.WriteLine($"{chexMix.GetName()}.");
-            break;
-        
-        case 5:
-            Console.WriteLine($"{pepsi.GetName()}.");
-            break;
-        case 6:
-            DisplayTotals();
-            break;
-        
-        default:
-        Console.WriteLine("Invalid input");
-        break;
+            }
+        }else {
+            Console.WriteLine("Invalid input");
         }
-
-
     }
+
     public static bool ValidateCoin(double CoinPaid){
         double[] money = {.01, .05, .10, .25, 1.00, 5.00};
         
@@ -64,6 +72,7 @@ class Program{
         }
         return false;
     }
+
     public static bool CheckAmountPaid(double sellingPrice, double[] paid){
         double TotalPaid = 0;
         
@@ -73,29 +82,44 @@ class Program{
         return TotalPaid >= sellingPrice;
     }
 
-//each time item is sold add to array, can use array.length to get total sales and return price for calculating profit
+//each time item is sold add to list, can use list.length to get total sales and return price for calculating profit
 //
-    public static void CalculateCost(double SellingPrice){
-        double cost = SellingPrice * 0.35;
+    public static double CalculateCost(double SellingPrice){
+        const double CostFactor = 0.35;
+        double cost = SellingPrice * CostFactor;
+
+        return cost;
     }
 
-    public static void CalculateOverhead(double SellingPrice){
-        double laborCost = SellingPrice * 0.05;
+    public static double CalculateOverhead(double SellingPrice){
+        const double OverheadFactor = 0.05;
+        double laborCost = SellingPrice * OverheadFactor;
+
+        return laborCost;
     }
+
     public static void CalculateProfit(){
 
     }
 
-     public static void CalculateLaborCost(double SellingPrice){
-        double laborCost = SellingPrice * 0.25;
+     public static double CalculateLaborCost(double SellingPrice){
+        const double FactorLabor = 0.25;
+        double laborCost = SellingPrice * FactorLabor;
+
+        return laborCost;
     }
 
-     public static void CalculateTotalSales(){
-        
+     public static int CalculateTotalSales(List<Item> items){
+        return items.Count;
     }
 
     public static void DisplayTotals(){
         // display total cost, labor cost, overhead, profit, total sales, total sales per item
+        // CalculateCost();
+        // CalculateLaborCost();
+        // CalculateOverhead();
+        // CalculateProfit();
+        // CalculateTotalSales();
     }
 
 }
